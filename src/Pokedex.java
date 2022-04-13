4f1a238339f5988053090214efd5de53a35f19fb
Osman <osman_suastegui@hotmail.com>
2022-04-12 23:11:55 -0600

    metodos agregados en pokedex

--- src/Pokedex.java
+++ src/Pokedex.java
@@ -1,4 +1,27 @@
 
 public class Pokedex extends Catalogo {
+    int totalPokemon = 0;
+    int limite = 3 ;
+    Pokemon [] misPokemon = new Pokemon[limite];
+
+    @Override
+    public int existePokemon(String nombre) {
+		for(int i = 0 ; i < this.totalPokemon; i++) {
+			if(misPokemon[i].nombre.equals(nombre)) {
+				return i;
+			}
+		}
+		return -1;
+	}
     
+    @Override
+    public Pokemon buscarPokemon(int i) {
+		return misPokemon[i];
+	}
+    public void agregarPokemon(Pokemon pokemonCapturado){
+        this.misPokemon[this.totalPokemon] = pokemonCapturado;
+        this.totalPokemon++;
+
+    }
+
 }
