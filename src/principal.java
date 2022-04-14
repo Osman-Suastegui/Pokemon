627449ba1db5ec1869997be4de70650f9a1cf04b
Osman <osman_suastegui@hotmail.com>
2022-04-13 23:39:00 -0600

    menu arreglado

--- src/principal.java
+++ src/principal.java
@@ -9,14 +9,15 @@ public class principal {
 		System.out.print("Ingrese su nombre: " );
 		nombre = sc.next();
 		Entrenador ent = new Entrenador(nombre);
-		System.out.println("¿Que desea hacer?");
-		System.out.println("1: Abrir menu catalogo");
-		System.out.println("2: Abrir Pokedex");
-		System.out.println("3: Caminar");
-		System.out.println("4: Salir del juego");
-		int opcion = sc.nextInt();
+		int opcion = 0;
 
 		while (opcion != 4) {
+			System.out.println("ï¿½Que desea hacer?");
+			System.out.println("1: Abrir menu catalogo");
+			System.out.println("2: Abrir Pokedex");
+			System.out.println("3: Caminar");
+			System.out.println("4: Salir del juego");
+			 opcion = sc.nextInt();
 			switch (opcion) {
 			case 1:
 				mostrarCatalogo();
@@ -38,7 +39,7 @@ public class principal {
 		}
 	}
 	
-	public static void mostrarCatalogo() {
+	public static int mostrarCatalogo() {
 		Scanner sc = new Scanner(System.in);
 		Catalogo catalogo = new Catalogo();
 		int opcion = 0;
@@ -51,41 +52,41 @@ public class principal {
 
 			opcion = sc.nextInt();
 			switch(opcion) {
-			case 1:
-				System.out.print("Ingrese el nombre del pokemon: ");
-				String nombre = sc.next();
-				int posicion = catalogo.existePokemon(nombre);
-				if(posicion == -1) {
-					System.out.println("El pokemon " + nombre + " no se encuentra");
-				}else {
-					catalogo.buscarPokemon(posicion).obtenerEstadisticas();
-				}
-				break;
-			case 2:
-				System.out.print("Ingrese el tipo del pokemon: ");
-				String tipo = sc.next();
-				Pokemon[] pok_tipo = catalogo.buscarPokemonTipo(tipo);
-
-				for(int i = 0; i < pok_tipo.length; i++) {
-					if(pok_tipo[i] != null) {
-						pok_tipo[i].obtenerEstadisticas();	
+				case 1:
+					System.out.print("Ingrese el nombre del pokemon: ");
+					String nombre = sc.next();
+					int posicion = catalogo.existePokemon(nombre);
+					if(posicion == -1) {
+						System.out.println("El pokemon " + nombre + " no se encuentra");
+					}else {
+						catalogo.buscarPokemon(posicion).obtenerEstadisticas();
 					}
-					
-				}			
-				break;
-			case 3:
-				System.out.println(catalogo.verPokemon());
-				break;
-			case 4:
-				opcion = 4;
-				break;
-			default:
-				System.out.println("Opcion invalida");
-				break;
-			
-			}
+					break;
+				case 2:
+					System.out.print("Ingrese el tipo del pokemon: ");
+					String tipo = sc.next();
+					Pokemon[] pok_tipo = catalogo.buscarPokemonTipo(tipo);
+
+					for(int i = 0; i < pok_tipo.length; i++) {
+						if(pok_tipo[i] != null) {
+							pok_tipo[i].obtenerEstadisticas();	
+						}
+						
+					}			
+					break;
+				case 3:
+					System.out.println(catalogo.verPokemon());
+					break;
+				case 4:
+					return 0;
+				default:
+					System.out.println("Opcion invalida");
+					break;
+				
+				}
 		}
 		sc.close();
+		return 0;
 		
 		
 	}
