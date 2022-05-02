210355b2693af6bb489786399362970820cf2f19
Osman <osman_suastegui@hotmail.com>
2022-05-02 16:50:19 -0600

    archivos .class eliminadas

--- src/Pokedex.java
+++ src/Pokedex.java
@@ -55,7 +55,7 @@ public class Pokedex extends Catalogo {
 	return str;	
 	}
 	public boolean estaLlena() {
-		return this.obtenerTotalPokemones() < this.limite;
+		return this.obtenerTotalPokemones() == this.limite ;
 	}    
    
     
